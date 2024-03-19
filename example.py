43f62747bc9233d1151a4f9eea9e14f55f459411
mishaanikutin <billyknife@mail.ru>
2024-03-19 17:57:10 +0300

    Добавил данные за февраль

--- example.py
+++ example.py
@@ -1,11 +1,14 @@
 from RZD_Parser import RZDParser
 
-start_date = '01.01.2014'
-end_date = '01.01.2024'
+start_date = '01.02.2022'
+#end_date = '01.04.2022'
 
 parser = RZDParser()
 
-df = parser.parse_data(date_publication_0=start_date, date_publication_1=end_date)
+df = parser.parse_data(
+    date_publication_0=start_date,
+    # date_publication_1=end_date
+)
 df.to_csv('example_parsed_data.csv')
 
 df = parser.update_data(csv_filepath='example_parsed_data.csv')
